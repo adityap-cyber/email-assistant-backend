10fa9be8638a26f07ace36eac0ae894a1cd56028
adityap-cyber <mradityap33@gmail.com>
2025-11-25 06:58:53 +0530

    .

--- src/main/java/com/email/writer/app/EmailRequest.java
+++ src/main/java/com/email/writer/app/EmailRequest.java
@@ -1,8 +1,12 @@
 package com.email.writer.app;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor        // 👈 REQUIRED for JSON binding
+@AllArgsConstructor
 public class EmailRequest {
     private String emailContent;
     private String tone;
